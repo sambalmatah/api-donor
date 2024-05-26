ded5b334c7a9442da4bf676f8dcc3d89d712f998
sambalmatah <yudiarinugroho14@gmail.com>
2024-05-27 00:18:43 +0700

    menambah-fungsi-crud-db-myqsl

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -19,34 +19,59 @@ const getAllUsers = async (request, response) => {
     }
 };
 
-const createNewUser = (request, response) => {
-    console.log(request.body)
-    response.json({
-        message: 'CREATE new user succes',
-        data: request.body
-    })
+const createNewUser = async (request, response) => {
+    const { body } = request;
+
+    try {
+        await usersModels.createNewUser(body);
+        response.json({
+            message: 'CREATE new user succes',
+            data: request.body
+        })
+    } catch (error) {
+        response.status(500).json({
+            message: 'Server Error',
+            serverMassage: error
+        })
+    }
 };
 
-const updateUser = (request, response) => {
+const updateUser = async (request, response) => {
     const { id } = request.params;
-    console.log(id);
-    response.json({
-        message: 'UPDATE user success',
-        data: request.body
-    })
+    const { body } = request;
+
+    try {
+        await usersModels.updateUser(body, id);
+        response.json({
+            message: 'UPDATE user success',
+            data: {
+                id: id,
+                ...body
+            }
+        })
+    } catch (error) {
+        response.status(500).json({
+            message: 'Server Error',
+            serverMassage: error
+        })
+    }
 };
 
-const deleteUser = (request, response) => {
+const deleteUser = async (request, response) => {
     const { id } =request.params;
-    console.log(id);
-    response.json({
-        message: 'DELETE user success',
-        data: {
-            id: id,
-            name: "yudi",
-            email: "yudi@gmail.com"
-        }
-    })
+
+    try {
+        await usersModels.deleteUser(id)
+        response.json({
+            message: 'DELETE user success',
+            data: null
+        })
+    } catch (error) {
+        response.status(500).json({
+            message: 'Server Error',
+            serverMassage: error
+        })
+    }
 } 
 
 module.exports = {
--- src/models/users.js
+++ src/models/users.js
@@ -6,6 +6,31 @@ const getAllUsers = () => {
     return dbPool.execute(SQLQuery);
 }
 
+const createNewUser = (body) => {
+    const SQLQuery = `  INSERT INTO users (nama, email, nik, password, status)
+                        VALUES ('${body.nama}', '${body.email}', '${body.nik}', '${body.password}', '${body.status}')`;
+
+    return dbPool.execute(SQLQuery);
+}
+
+const updateUser = (body, id) => {
+    const SQLQuery = `  UPDATE users
+                        SET nama='${body.nama}', email='${body.email}', nik='${body.nik}'
+                        WHERE id='${id}'`
+
+    return dbPool.execute(SQLQuery);
+}
+
+const deleteUser = (id) => {
+    const SQLQuery = `  DELETE FROM users
+                        WHERE id=${id}`;
+
+    return dbPool.execute(SQLQuery);
+}
+
 module.exports = {
     getAllUsers,
+    createNewUser,
+    updateUser,
+    deleteUser,
 }
