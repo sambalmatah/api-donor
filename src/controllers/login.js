010d985b5daaabeadc3f18f38c06f459d73c7e03
Mildan Falah <mildanfalah@gmail.com>
2024-06-07 15:28:06 +0700

    fix merge

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const loginModels = require("../models/login.js");
 
 const loginUser = async (request, response) => {
@@ -29,37 +28,3 @@ const loginUser = async (request, response) => {
 module.exports = {
   loginUser,
 };
-=======
-const loginModels = require('../models/login.js');
-
-const loginUser = async (request, response) => {
-    const { email, password } = request.body;
-
-    try {
-        const [ data ] = await loginModels.loginUser(email, password);
-        
-        if (!data) {
-            response.status(400).json({
-                message: "Login Invalid",
-                data: null,
-            })
-        }
-
-        response.json({
-            message: "login berhasil",
-            data: data
-        })
-    } catch (error) {
-        response.status(500).json({
-            message: "Server Error",
-            serverMessage: error
-        })
-    }
-}
-
-
-
-module.exports = {
-    loginUser,
-}
->>>>>>> f404151f18ff7303a2e9ae3de01b15cf6c2f632a
